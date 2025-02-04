fc34fdd9be8c4c9715a045b07bd315ff8ede78cf
Yongjin <144215228+YongJin04@users.noreply.github.com>
2025-02-05 06:19:11 +0900

    Implementing Import Table and Export Table Parsing Functions.

--- pe_parser.c
+++ pe_parser.c
@@ -10,7 +10,10 @@ IMAGE_DOS_HEADER gDosHdr;
 IMAGE_FILE_HEADER gFileHdr;
 IMAGE_OPTIONAL_HEADER32 gOptionalHdr32;
 IMAGE_OPTIONAL_HEADER64 gOptionalHdr64;
-IMAGE_DATA_DIRECTORY gDataDirectory;
+IMAGE_DATA_DIRECTORY gDataDirectory[16];
+IMAGE_SECTION_HEADER* gSectionHeaders = NULL;
+IMAGE_IMPORT_DESCRIPTOR gImportDescriptor;
+IMAGE_EXPORT_DIRECTORY gExportDirectory;
 
 static void processFile(const char*);
 static void parseDosHeader(FILE*);
@@ -18,6 +21,13 @@ static void parseDosStub(FILE*);
 static void showAsciiStub(FILE*, long, long);
 static void parseNtHeader(FILE*);
 static char* convertTime(DWORD);
+static void parseSectionHeader(FILE*);
+static void parseSection(FILE*);
+static void parseImportAddressTable32(FILE*);
+static void parseImportAddressTable64(FILE*);
+static void parseExportAddressTable32(FILE*);
+static void parseExportAddressTable64(FILE*);
+char* readNullTerminatedString(FILE*);
 
 int main(int argc, char* argv[]) {
     if (argc != 3 || strcmp(argv[1], "-f") != 0) {
@@ -40,7 +50,11 @@ static void processFile(const char* path) {
     parseDosHeader(filePtr);
     parseDosStub(filePtr);
     parseNtHeader(filePtr);
+    parseSectionHeader(filePtr);
 
+    parseSection(filePtr);
+
+    free(gSectionHeaders);
     fclose(filePtr);
 }
 
@@ -135,7 +149,6 @@ static char* convertTime(DWORD t) {
     return buf;
 }
 
-
 static void parseNtHeader(FILE* filePtr) {
     fseek(filePtr, gDosHdr.AddressOfNewExeHeader, SEEK_SET);
     DWORD ntSig;
@@ -159,9 +172,9 @@ static void parseNtHeader(FILE* filePtr) {
     printf("     - File Header\n");
     printf("         - Machine                   : 0x%s\n", getMachineName(gFileHdr.Machine));
     printf("         - Number of Sections        : 0x%X\n", gFileHdr.NumberOfSections);
-    printf("         - TimeDateStamp             : %s\n", convertTime(gFileHdr.TimeDateStamp));
-    printf("         - PointerToSymbolTable      : 0x%X\n", gFileHdr.PointerToSymbolTable);
-    printf("         - NumberOfSymbols           : 0x%X\n", gFileHdr.NumberOfSymbols);
+    printf("         - Time DateStamp            : %s\n", convertTime(gFileHdr.TimeDateStamp));
+    printf("         - Pointer To SymbolTable    : 0x%X\n", gFileHdr.PointerToSymbolTable);
+    printf("         - Number Of Symbols         : 0x%X\n", gFileHdr.NumberOfSymbols);
     printf("         - Size of Optional Header   : 0x%X\n", gFileHdr.SizeOfOptionalHeader);
     printf("         - Characteristics           : 0x%X\n", gFileHdr.Characteristics);
     showFileCharacteristics(gFileHdr.Characteristics);
@@ -173,10 +186,7 @@ static void parseNtHeader(FILE* filePtr) {
         return;
     }
     fseek(filePtr, -static_cast<long>(sizeof(WORD)), SEEK_CUR);
-    // Compair Optional Header Signature Value.
     if (optionalHeaderSignature == 0x010B) {
-        // x86
-        printf("%llX\n", sizeof(IMAGE_OPTIONAL_HEADER32));
         if (fread(&gOptionalHdr32, sizeof(IMAGE_OPTIONAL_HEADER32), 1, filePtr) != 1) {
             perror("Error reading Optional Header x86 Signature of NT header");
             return;
@@ -215,18 +225,16 @@ static void parseNtHeader(FILE* filePtr) {
         printf("         - Number Of RVA And Sizes   : 0x%X\n", gOptionalHdr32.NumberOfRvaAndSizes);
         printf("         - Data Directory\n");
         for (DWORD i = 0; i < gOptionalHdr32.NumberOfRvaAndSizes; i++) {
-            if (fread(&gDataDirectory, sizeof(IMAGE_DATA_DIRECTORY), 1, filePtr) != 1) {
+            if (fread(&gDataDirectory[i], sizeof(IMAGE_DATA_DIRECTORY), 1, filePtr) != 1) {
                 perror("Error Reading Dos Header : ");
                 break;
             }
-            if (gDataDirectory.VirtualAddress != 0x00000000) {
-                printf("             - %s (RVA : 0x%X / Virtual Size : 0x%X)\n", directoryNames[i], gDataDirectory.VirtualAddress, gDataDirectory.Size);
+            if (gDataDirectory[i].VirtualAddress != 0x00000000) {
+                printf("             - %s (RVA : 0x%X / Virtual Size : 0x%X)\n", directoryNames[i], gDataDirectory[i].VirtualAddress, gDataDirectory[i].Size);
             }
         }
-
     }
     else if (optionalHeaderSignature == 0x020B) {
-        // x86-64
         if (fread(&gOptionalHdr64, sizeof(IMAGE_OPTIONAL_HEADER64), 1, filePtr) != 1) {
             perror("Error reading Optional Header x86-64 Signature of NT header");
             return;
@@ -264,13 +272,255 @@ static void parseNtHeader(FILE* filePtr) {
         printf("         - Number Of RVA And Sizes   : 0x%X\n", gOptionalHdr64.NumberOfRvaAndSizes);
         printf("         - Data Directory\n");
         for (DWORD i = 0; i < gOptionalHdr64.NumberOfRvaAndSizes; i++) {
-            if (fread(&gDataDirectory, sizeof(IMAGE_DATA_DIRECTORY), 1, filePtr) != 1) {
+            if (fread(&gDataDirectory[i], sizeof(IMAGE_DATA_DIRECTORY), 1, filePtr) != 1) {
                 perror("Error Reading Dos Header : ");
                 break;
             }
-            if (gDataDirectory.VirtualAddress != 0x00000000) {
-                printf("             - %s (RVA : 0x%X / Virtual Size : 0x%X)\n", directoryNames[i], gDataDirectory.VirtualAddress, gDataDirectory.Size);
+            if (gDataDirectory[i].VirtualAddress != 0x00000000) {
+                printf("             - %s (RVA : 0x%X / Virtual Size : 0x%X)\n", directoryNames[i], gDataDirectory[i].VirtualAddress, gDataDirectory[i].Size);
             }
         }
     }
+}
+
+static void parseSectionHeader(FILE* filePtr) {
+    printf("\n - Section Header\n");
+
+    gSectionHeaders = (IMAGE_SECTION_HEADER*)malloc(gFileHdr.NumberOfSections * sizeof(IMAGE_SECTION_HEADER));
+    if (gSectionHeaders == NULL) {
+        perror("Memory allocation failed for section headers");
+        return;
+    }
+
+    for (int i = 0; i < gFileHdr.NumberOfSections; i++) {
+        if (fread(&gSectionHeaders[i], sizeof(IMAGE_SECTION_HEADER), 1, filePtr) != 1) {
+            perror("Error Reading Section Header");
+            free(gSectionHeaders);
+            gSectionHeaders = NULL;
+            return;
+        }
+
+        printf("     - Name                          : %.8s\n", gSectionHeaders[i].Name);
+        printf("     - Virtual Size                  : 0x%X\n", gSectionHeaders[i].VirtualSize);
+        printf("     - Virtual Address               : 0x%X\n", gSectionHeaders[i].VirtualAddress);
+        printf("     - Size of Raw Data              : 0x%X\n", gSectionHeaders[i].SizeOfRawData);
+        printf("     - Pointer to Raw Data           : 0x%X\n", gSectionHeaders[i].PointerToRawData);
+        printf("     - Pointer to Relocations        : 0x%X\n", gSectionHeaders[i].PointerToRelocations);
+        printf("     - Pointer to Line Numbers       : 0x%X\n", gSectionHeaders[i].PointerToLinenumbers);
+        printf("     - Number of Relocations         : 0x%X\n", gSectionHeaders[i].NumberOfRelocations);
+        printf("     - Number of Line Numbers        : 0x%X\n", gSectionHeaders[i].NumberOfLinenumbers);
+        printf("     - Characteristics               : 0x%X\n", gSectionHeaders[i].Characteristics);
+        showSectionCharacteristics(gSectionHeaders[i].Characteristics);
+        printf("\n");
+    }
+}
+
+static void parseSection(FILE* filePtr) {
+    printf(" - Section\n");
+
+    // IAT Parsing
+    if (gOptionalHdr32.Magic == 0x010B) {
+        parseImportAddressTable32(filePtr);
+        parseExportAddressTable32(filePtr);
+    }
+    else if (gOptionalHdr64.Magic == 0x020B) {
+        parseImportAddressTable64(filePtr);
+        parseExportAddressTable64(filePtr);
+    }
+}
+
+static void parseImportAddressTable32(FILE* filePtr) {
+    for (int i = 0; i < gFileHdr.NumberOfSections; i++) {
+        if ((gSectionHeaders[i].VirtualAddress <= gDataDirectory[12].VirtualAddress) && (gDataDirectory[12].VirtualAddress < gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].VirtualSize)) {
+            printf("     - %.8s\n", gSectionHeaders[i].Name);
+            printf("         - Import Descriptor\n");
+
+            QWORD current_point = gSectionHeaders[i].PointerToRawData + gDataDirectory[12].Size;
+            while (1) {
+                fseek(filePtr, current_point, SEEK_SET);
+                if (fread(&gImportDescriptor, sizeof(IMAGE_IMPORT_DESCRIPTOR), 1, filePtr) != 1) {
+                    perror("Error Import Descriptorr");
+                    return;
+                }
+
+                current_point = ftell(filePtr);
+                if (gImportDescriptor.OriginalFirstThunk == 0x00000000) {
+                    break;
+                }
+
+                fseek(filePtr, (gImportDescriptor.Name - gSectionHeaders[i].VirtualAddress) + gSectionHeaders[i].PointerToRawData, SEEK_SET);
+                char* name = readNullTerminatedString(filePtr);
+                printf("             - DLL Name : %s\n", name);
+
+                QWORD current_point_1 = (gImportDescriptor.FirstThunk - gSectionHeaders[i].VirtualAddress) + gSectionHeaders[i].PointerToRawData;
+                while (1) {
+                    fseek(filePtr, current_point_1, SEEK_SET);
+                    DWORD temp_ptr;
+                    if (fread(&temp_ptr, sizeof(DWORD), 1, filePtr) != 1) {
+                        perror("Error Import Descriptorr");
+                        return;
+                    }
+
+                    current_point_1 = ftell(filePtr);
+                    if (temp_ptr == 0x00000000) {
+                        break;
+                    }
+
+                    fseek(filePtr, (temp_ptr - gSectionHeaders[i].VirtualAddress) + gSectionHeaders[i].PointerToRawData + 0x02, SEEK_SET);
+                    char* name = readNullTerminatedString(filePtr);
+                    printf("                 - Import Symbol Name : %s\n", name);
+                }
+                printf("\n");
+            }
+            break;
+        }
+    }
+}
+
+static void parseImportAddressTable64(FILE* filePtr) {
+    for (int i = 0; i < gFileHdr.NumberOfSections; i++) {
+        if ((gSectionHeaders[i].VirtualAddress <= gDataDirectory[12].VirtualAddress) && (gDataDirectory[12].VirtualAddress < gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].VirtualSize)) {
+            printf("     - %.8s\n", gSectionHeaders[i].Name);
+            printf("         - Import Descriptor\n");
+
+            QWORD current_point = gSectionHeaders[i].PointerToRawData + gDataDirectory[12].Size;
+            while (1) {
+                fseek(filePtr, current_point, SEEK_SET);
+                if (fread(&gImportDescriptor, sizeof(IMAGE_IMPORT_DESCRIPTOR), 1, filePtr) != 1) {
+                    perror("Error Import Descriptorr");
+                    return;
+                }
+
+                current_point = ftell(filePtr);
+                if (gImportDescriptor.OriginalFirstThunk == 0x0000000000000000) {
+                    break;
+                }
+
+                fseek(filePtr, (gImportDescriptor.Name - gSectionHeaders[i].VirtualAddress) + gSectionHeaders[i].PointerToRawData, SEEK_SET);
+                char* name = readNullTerminatedString(filePtr);
+                printf("             - DLL Name : %s\n", name);
+
+                QWORD current_point_1 = (gImportDescriptor.FirstThunk - gSectionHeaders[i].VirtualAddress) + gSectionHeaders[i].PointerToRawData;
+                while (1) {
+                    fseek(filePtr, current_point_1, SEEK_SET);
+                    QWORD temp_ptr;
+                    if (fread(&temp_ptr, sizeof(QWORD), 1, filePtr) != 1) {
+                        perror("Error Import Descriptorr");
+                        return;
+                    }
+
+                    current_point_1 = ftell(filePtr);
+                    if (temp_ptr == 0x00000000) {
+                        break;
+                    }
+
+                    fseek(filePtr, (temp_ptr - gSectionHeaders[i].VirtualAddress) + gSectionHeaders[i].PointerToRawData + 0x02, SEEK_SET);
+                    char* name = readNullTerminatedString(filePtr);
+                    printf("                 - Import Symbol Name : %s\n", name);
+                }
+                printf("\n");
+            }
+            break;
+        }
+    }
+}
+
+static void parseExportAddressTable32(FILE* filePtr) {
+    for (int i = 0; i < gFileHdr.NumberOfSections; i++) {
+        if ((gSectionHeaders[i].VirtualAddress <= gDataDirectory[0].VirtualAddress) && (gDataDirectory[0].VirtualAddress < gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].VirtualSize)) {
+            printf("     - %.8s\n", gSectionHeaders[i].Name);
+            printf("         - Export Descriptor\n");
+
+            QWORD current_point = gDataDirectory[0].VirtualAddress - gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].PointerToRawData;
+
+            fseek(filePtr, current_point, SEEK_SET);
+            if (fread(&gExportDirectory, sizeof(IMAGE_EXPORT_DIRECTORY), 1, filePtr) != 1) {
+                perror("Error Export Directory");
+                return;
+            }
+
+            current_point = gExportDirectory.Name - gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].PointerToRawData;
+            fseek(filePtr, current_point, SEEK_SET);
+            char* name = readNullTerminatedString(filePtr);
+            printf("             - DLL Name : %s\n", name);
+
+            QWORD current_point_1 = gExportDirectory.AddressOfNames - gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].PointerToRawData;
+            fseek(filePtr, current_point_1, SEEK_SET);
+            for (int j = 0; j < gExportDirectory.NumberOfNames;j++) {
+                fseek(filePtr, current_point_1, SEEK_SET);
+                DWORD temp_ptr;
+                if (fread(&temp_ptr, sizeof(DWORD), 1, filePtr) != 1) {
+                    perror("Error Import Descriptorr");
+                    return;
+                }
+                current_point_1 = ftell(filePtr);
+
+                fseek(filePtr, temp_ptr - gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].PointerToRawData, SEEK_SET);
+                char* name = readNullTerminatedString(filePtr);
+                printf("                 - Export Symbol Name : %s\n", name);
+            }
+            printf("\n");
+        }
+    }
+}
+
+static void parseExportAddressTable64(FILE* filePtr) {
+    for (int i = 0; i < gFileHdr.NumberOfSections; i++) {
+        if ((gSectionHeaders[i].VirtualAddress <= gDataDirectory[0].VirtualAddress) && (gDataDirectory[0].VirtualAddress < gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].VirtualSize)) {
+            printf("     - %.8s\n", gSectionHeaders[i].Name);
+            printf("         - Export Descriptor\n");
+
+            QWORD current_point = gDataDirectory[0].VirtualAddress - gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].PointerToRawData;
+
+            fseek(filePtr, current_point, SEEK_SET);
+            if (fread(&gExportDirectory, sizeof(IMAGE_EXPORT_DIRECTORY), 1, filePtr) != 1) {
+                perror("Error Export Directory");
+                return;
+            }
+
+            current_point = gExportDirectory.Name - gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].PointerToRawData;
+            fseek(filePtr, current_point, SEEK_SET);
+            char* name = readNullTerminatedString(filePtr);
+            printf("             - DLL Name : %s\n", name);
+
+            QWORD current_point_1 = gExportDirectory.AddressOfNames - gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].PointerToRawData;
+            fseek(filePtr, current_point_1, SEEK_SET);
+            for (int j = 0; j < gExportDirectory.NumberOfNames;j++) {
+                fseek(filePtr, current_point_1, SEEK_SET);
+                DWORD temp_ptr;
+                if (fread(&temp_ptr, sizeof(DWORD), 1, filePtr) != 1) {
+                    perror("Error Import Descriptorr");
+                    return;
+                }
+                current_point_1 = ftell(filePtr);
+
+                fseek(filePtr, temp_ptr - gSectionHeaders[i].VirtualAddress + gSectionHeaders[i].PointerToRawData, SEEK_SET);
+                char* name = readNullTerminatedString(filePtr);
+                printf("                 - Export Symbol Name : %s\n", name);
+            }
+            printf("\n");
+        }
+    }
+}
+
+char* readNullTerminatedString(FILE* filePtr) {
+    char* name = NULL;
+    size_t length = 0;
+    char ch;
+
+    while (fread(&ch, 1, 1, filePtr) == 1 && ch != '\0') {
+        char* temp = (char*)realloc(name, length + 2);
+        if (!temp) {
+            free(name);
+            return NULL;
+        }
+        name = temp;
+        name[length++] = ch;
+    }
+
+    if (name) {
+        name[length] = '\0';
+    }
+
+    return name;
 }
